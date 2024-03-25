38a7cb02d5c3b811241819a61c5fe37aae8d47e0
webtides9542 <164920851+webtides9542@users.noreply.github.com>
2024-03-25 22:58:46 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,95 @@
+
+
+$(document).ready(function () {
+    //Owl
+    $('.hero-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        navText: ['PREV', 'NEXT'],
+        smartSpeed: 1000,
+        autoplay: true,
+        autoplayTimeout: 7000,
+        responsive: {
+            0: {
+                nav: false,
+            },
+            768: {
+                nav: true,
+            }
+        }
+    })
+
+    $('#projects-slider').owlCarousel({
+        loop: true,
+        nav: false,
+        items: 2,
+        dots: true,
+        smartSpeed: 600,
+        center: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2,
+                margin: 8,
+            }
+        }
+    })
+
+    $('.reviews-slider').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        smartSpeed: 900,
+        items: 1,
+        margin: 24,
+        autoplay: true,
+        autoplayTimeout: 4000,
+    })
+
+
+    let slider = document.querySelector('.slider .list');
+    let items = document.querySelectorAll('.slider .list .item');
+    let next = document.getElementById('next');
+    let prev = document.getElementById('prev');
+    let dots = document.querySelectorAll('.slider .dots li');
+
+    let lengthItems = items.length - 1;
+    let active = 0;
+    next.onclick = function () {
+        active = active + 1 <= lengthItems ? active + 1 : 0;
+        reloadSlider();
+    }
+    prev.onclick = function () {
+        active = active - 1 >= 0 ? active - 1 : lengthItems;
+        reloadSlider();
+    }
+    let refreshInterval = setInterval(() => { next.click() }, 3000);
+    function reloadSlider() {
+        slider.style.left = -items[active].offsetLeft + 'px';
+        // 
+        let last_active_dot = document.querySelector('.slider .dots li.active');
+        last_active_dot.classList.remove('active');
+        dots[active].classList.add('active');
+
+        clearInterval(refreshInterval);
+        refreshInterval = setInterval(() => { next.click() }, 3000);
+
+
+    }
+
+    dots.forEach((li, key) => {
+        li.addEventListener('click', () => {
+            active = key;
+            reloadSlider();
+        })
+    })
+    window.onresize = function (event) {
+        reloadSlider();
+    };
+});
